f7ee3b748f71e85fed3a0110957a92ed31e90f90
Paweł Bulwan <git@taurit.pl>
2018-08-08 20:55:00 +0200

    Initial commit

---
+++ Taurit.Config.Catalog/Controllers/HealthyMealPlannerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Taurit.Config.Catalog.Models.HealthyMealPlanner;
+
+namespace Taurit.Config.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthyMealPlannerController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("CurrentVersion")]
+        public ActionResult<SoftwareVersionInformation> Get()
+        {
+            // mock
+            var currentVersion = new SoftwareVersionInformation(new Version(0, 0, 1));
+            return currentVersion;
+        }
+    }
+}
---
+++ Taurit.Config.Catalog/Models/HealthyMealPlanner/SoftwareVersionInformation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Taurit.Config.Catalog.Models.HealthyMealPlanner
+{
+    public class SoftwareVersionInformation
+    {
+        [JsonProperty]
+        public Version Version { get; private set; }
+
+        [Obsolete("Do not use unless you are a deserializer")]
+        private SoftwareVersionInformation()
+        {
+        }
+
+        public SoftwareVersionInformation(Version version)
+        {
+            Version = version;
+        }
+    }
+}
